134127eea49e0c5d23dc7d98c27c0b7a045ab760
bartosz99 <62900319+bartosz99@users.noreply.github.com>
2024-08-03 10:40:37 +0200

    code cleanup and improvments

--- components/CustomerCard.vue
+++ components/CustomerCard.vue
@@ -6,78 +6,53 @@ type Props = {
 
 defineProps<Props>();
 
-const rotateX = ref('0deg');
-const rotateY = ref('0deg');
-const mouseX = ref('0%');
-const mouseY = ref('0%');
-const backgroundX = ref('50%');
-const backgroundY = ref('50%');
-
+const mouseX = ref(0);
+const mouseY = ref(0);
 const opacity = ref(0);
 const isTransitioning = ref(false);
 
+const backgroundX = computed(() => 50 + (mouseX.value / 100) * 20);
+const backgroundY = computed(() => 50 + (mouseY.value / 100) * 20);
+const rotateX = computed(() => 6 - (mouseX.value / 100) * 12);
+const rotateY = computed(() => -11 + (mouseY.value / 100) * 22);
+
 const handleCursorMove = (e: any) => {
-  const mouseXPercent = (e.offsetX / e.srcElement.offsetWidth) * 100;
-  const mouseYPercent = (e.offsetY / e.srcElement.offsetHeight) * 100;
-
-  const backgroundXPercent = 50 + (mouseXPercent / 100) * 20;
-  const backgroundYPercent = 50 + (mouseYPercent / 100) * 20;
-
-  const rotateXDegrees = 6 - (mouseXPercent / 100) * 12;
-  const rotateYDegrees = -11 + (mouseYPercent / 100) * 22;
-
-  mouseX.value = mouseXPercent.toFixed(2) + '%';
-  mouseY.value = mouseYPercent.toFixed(2) + '%';
-
-  backgroundX.value = backgroundXPercent.toFixed(2) + '%';
-  backgroundY.value = backgroundYPercent.toFixed(2) + '%';
-
-  rotateX.value = rotateXDegrees.toFixed(2) + 'deg';
-  rotateY.value = rotateYDegrees.toFixed(2) + 'deg';
+  mouseX.value = Number(((e.offsetX / e.srcElement.offsetWidth) * 100).toFixed(2));
+  mouseY.value = Number(((e.offsetY / e.srcElement.offsetHeight) * 100).toFixed(2));
 };
 
-const handleMouseEnter = () => {
-  opacity.value = 0.6;
+const handleTransitionEnabling = (newOpacity: number) => {
+  opacity.value = newOpacity;
   isTransitioning.value = true;
   setTimeout(() => {
     isTransitioning.value = false;
   }, 300);
-};
-
-const handleMouseLeave = () => {
-  opacity.value = 0;
-  isTransitioning.value = true;
-  setTimeout(() => {
-    isTransitioning.value = false;
-  }, 300);
-  rotateX.value = '0deg';
-  rotateY.value = '0deg';
 };
 </script>
 
 <template>
   <div
-    class="card relative border border-slate-900 min-w-80 w-80 overflow-hidden cursor-pointer onHover"
-    :class="{ 'transition-transform duration-300': isTransitioning }"
+    class="card relative border border-slate-900 min-w-80 w-80 overflow-hidden cursor-pointer"
+    :class="{ 'card-transition': isTransitioning }"
     :style="{
-      '--r-x': rotateX,
-      '--r-y': rotateY,
-      '--m-x': mouseX,
-      '--m-y': mouseY,
-      '--bg-x': backgroundX,
-      '--bg-y': backgroundY,
+      '--r-x': `${rotateX}deg`,
+      '--r-y': `${rotateY}deg`,
+      '--m-x': `${mouseX}%`,
+      '--m-y': `${mouseY}%`,
+      '--bg-x': `${backgroundX}%`,
+      '--bg-y': `${backgroundY}%`,
       '--opacity': opacity
     }"
     @mousemove="handleCursorMove($event)"
-    @mouseenter="handleMouseEnter"
-    @mouseleave="handleMouseLeave"
+    @mouseenter="handleTransitionEnabling(0.6)"
+    @mouseleave="handleTransitionEnabling(0)"
   >
-    <div class="parent">
+    <div class="content-wrapper">
       <div>
-        <img class="baseImage" :src="imageSource" :alt="imageAlt" />
+        <img :src="imageSource" :alt="imageAlt" />
       </div>
-      <div class="firstLayer" />
-      <div class="secondLayer" />
+      <div class="cursor-animation" />
+      <div class="card-animation" />
     </div>
   </div>
 </template>
@@ -87,11 +62,15 @@ const handleMouseLeave = () => {
   border-radius: 48px;
 }
 
+.card-transition {
+  transition: transform 300ms;
+}
+
 .card:hover {
   transform: perspective(800px) rotateY(var(--r-x)) rotateX(var(--r-y));
 }
 
-.parent {
+.content-wrapper {
   display: grid;
   will-change: transform;
   transform-origin: center center;
@@ -100,7 +79,7 @@ const handleMouseLeave = () => {
   border: 1px solid var(--color-border-primary);
 }
 
-.parent > * {
+.content-wrapper > * {
   width: 100%;
   height: 100%;
   display: grid;
@@ -108,9 +87,8 @@ const handleMouseLeave = () => {
   clip-path: inset(0 0 0 0 round var(--radius));
 }
 
-.firstLayer {
-  --radius: 20px;
-
+.cursor-animation {
+  --radius: 48px;
   mix-blend-mode: soft-light;
   clip-path: inset(0 0 1px 0 round var(--radius));
   opacity: var(--opacity);
@@ -124,26 +102,11 @@ const handleMouseLeave = () => {
   );
 }
 
-.secondLayer {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, var(--opacity));
-  transition: background 0.3s;
-  mix-blend-mode: color-dodge;
-  opacity: var(--opacity);
-  will-change: background;
-  transition-duration: ;
-  transition-timing-function: ;
-  transition-delay: ;
-  transition-behavior: ;
-  transition-property: opacity;
-  clip-path: inset(0 0 1px 0 round var(--radius));
+.card-animation {
   --foil-svg: url(../assets/images/card_backround_image.svg);
   --step: 5%;
   --pattern: var(--foil-svg) center/100% no-repeat;
+
   --rainbow: repeating-linear-gradient(
       0deg,
       rgb(255, 119, 115) calc(var(--step) * 1),
@@ -155,6 +118,7 @@ const handleMouseLeave = () => {
       rgb(255, 119, 115) calc(var(--step) * 7)
     )
     0% var(--bg-y) / 200% 700% no-repeat;
+
   --diagonal: repeating-linear-gradient(
       128deg,
       #0e152e 0%,
@@ -165,6 +129,7 @@ const handleMouseLeave = () => {
       #0e152e 12%
     )
     var(--bg-x) var(--bg-y) / 300% no-repeat;
+
   --shade: radial-gradient(
       farthest-corner circle at var(--m-x) var(--m-y),
       rgba(255, 255, 255, 0.1) 12%,
@@ -172,11 +137,20 @@ const handleMouseLeave = () => {
       rgba(255, 255, 255, 0.25) 120%
     )
     var(--bg-x) var(--bg-y) / 300% no-repeat;
+
+  transition: background 0.3s;
+  transition-property: opacity;
+  opacity: var(--opacity);
+
+  mix-blend-mode: color-dodge;
+  will-change: background;
+  clip-path: inset(0 0 1px 0 round var(--radius));
+
   background-blend-mode: hue, hue, hue, overlay;
   background: var(--pattern), var(--rainbow), var(--diagonal), var(--shade);
 }
 
-.secondLayer::after {
+.card-animation::after {
   content: '';
   mix-blend-mode: exclusion;
   background-blend-mode: soft-light, hue, hard-light;
